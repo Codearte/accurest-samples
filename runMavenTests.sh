f9039a479637265bf12ed59a8dd1eb47d63546d4
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-05-03 21:17:12 +0200

    Added more parametrization of scripts

--- runMavenTests.sh
+++ runMavenTests.sh
@@ -3,17 +3,18 @@
 set -o errexit
 
 LOCAL_MAVEN_REPO=${LOCAL_MAVEN_REPO-~/.m2}
+ACCUREST_VERSION=${ACCUREST_VERSION:-1.1.0-M4}
 ROOT=`pwd`
 
 echo -e "Clearing saved stubs"
 rm -rf $LOCAL_MAVEN_REPO/repository/io/codearte/accurest/testprojects/
 
 echo -e "\n\nRunning tests for Maven\n\n"
-echo -e "Building server"
+echo -e "Building server (uses Accurest Maven Plugin)"
 cd http-server
-./mvnw clean install -Daccurest.version=1.1.0-M4
+./mvnw clean install -Daccurest.version=${ACCUREST_VERSION}
 cd $ROOT
-echo -e "\n\nBuilding client"
+echo -e "\n\nBuilding client (uses Accurest Stub Runner)"
 cd http-client
-./mvnw clean package
+./mvnw clean package -Daccurest.version=${ACCUREST_VERSION}
 cd $ROOT
--- runTests.sh
+++ runTests.sh
@@ -3,17 +3,18 @@
 set -o errexit
 
 LOCAL_MAVEN_REPO=${LOCAL_MAVEN_REPO-~/.m2}
+ACCUREST_VERSION=${ACCUREST_VERSION:-1.1.0-M4}
 ROOT=`pwd`
 
 echo -e "Clearing saved stubs"
 rm -rf $LOCAL_MAVEN_REPO/repository/io/codearte/accurest/testprojects/
 
 echo -e "\n\nRunning tests for Gradle\n\n"
-echo -e "Building server"
+echo -e "Building server (uses Accurest Gradle Plugin)"
 cd http-server
-./gradlew clean build publishToMavenLocal
+./gradlew clean build publishToMavenLocal -PaccurestVersion=${ACCUREST_VERSION}
 cd $ROOT
-echo -e "\n\nBuilding client"
+echo -e "\n\nBuilding client (uses Accurest Stub Runner)"
 cd http-client
-./gradlew clean build
+./gradlew clean build -PaccurestVersion=${ACCUREST_VERSION}
 cd $ROOT
