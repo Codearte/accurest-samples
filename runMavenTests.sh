ffd0316e5f2068549658fbf0b65268ea0ecaf00b
Mariusz Smykula <mariuszs@gmail.com>
2016-05-03 18:06:50 +0200

    accurest version as parameter for testing

--- runMavenTests.sh
+++ runMavenTests.sh
@@ -8,10 +8,10 @@ ROOT=`pwd`
 echo -e "Clearing saved stubs"
 rm -rf $LOCAL_MAVEN_REPO/repository/io/codearte/accurest/testprojects/
 
-echo -e "\n\nRunning tests for Gradle\n\n"
+echo -e "\n\nRunning tests for Maven\n\n"
 echo -e "Building server"
 cd http-server
-./mvnw clean install
+./mvnw clean install -Daccurest.version=1.1.0-M4
 cd $ROOT
 echo -e "\n\nBuilding client"
 cd http-client
