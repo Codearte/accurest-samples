76d998af98278bdcbe2cbdda7caf306fdf949c53
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-06-19 23:55:44 +0200

    Polish

--- runMavenTests.sh
+++ runMavenTests.sh
@@ -3,24 +3,24 @@
 set -o errexit
 
 LOCAL_MAVEN_REPO=${LOCAL_MAVEN_REPO-~/.m2}
-ACCUREST_VERSION=${ACCUREST_VERSION:-1.0.0.BUILD-SNAPSHOT}
+VERIFIER_VERSION=${VERIFIER_VERSION:-1.0.0.BUILD-SNAPSHOT}
 ROOT=`pwd`
 
 cat <<EOF
 Running tests with the following parameters
 LOCAL_MAVEN_REPO=${LOCAL_MAVEN_REPO}
-ACCUREST_VERSION=${ACCUREST_VERSION}
+VERIFIER_VERSION=${VERIFIER_VERSION}
 EOF
 
 echo -e "\n\nClearing saved stubs"
 rm -rf $LOCAL_MAVEN_REPO/repository/org/springframework/cloud/contract/testprojects/
 
 echo -e "\n\nRunning tests for Maven\n\n"
-echo -e "Building server (uses Accurest Maven Plugin)"
+echo -e "Building server (uses Spring Cloud Contract Verifier Maven Plugin)"
 cd http-server
-./mvnw clean install -Daccurest.version=${ACCUREST_VERSION}
+./mvnw clean install -Daccurest.version=${VERIFIER_VERSION}
 cd $ROOT
-echo -e "\n\nBuilding client (uses Accurest Stub Runner)"
+echo -e "\n\nBuilding client (uses Spring Cloud Contract Stub Runner)"
 cd http-client
-./mvnw clean package -Daccurest.version=${ACCUREST_VERSION}
+./mvnw clean package -Daccurest.version=${VERIFIER_VERSION}
 cd $ROOT
